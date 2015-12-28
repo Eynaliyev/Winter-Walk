3723484d578ffd0f7f170308de8c85a813d547c6
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2015-12-29 02:11:40 +0400

    Fixed the Camera steoreo issue

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,7 +6,6 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 6f;            // The speed that the player will move at.
 
     Vector3 movement;                   // The vector to store the direction of the player's movement.
-    Animator anim;                      // Reference to the animator component.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
     int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
     float camRayLength = 100f;          // The length of the ray from the camera into the scene.
@@ -17,7 +16,6 @@ public class PlayerMovement : MonoBehaviour
         floorMask = LayerMask.GetMask ("Floor");
 
         // Set up references.
-        anim = GetComponent <Animator> ();
         playerRigidbody = GetComponent <Rigidbody> ();
     }
 
@@ -35,7 +33,6 @@ public class PlayerMovement : MonoBehaviour
         Turning ();
 
         // Animate the player.
-        Animating (h, v);
     }
 
     void Move (float h, float v)
@@ -75,12 +72,4 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void Animating (float h, float v)
-    {
-        // Create a boolean that is true if either of the input axes is non-zero.
-        bool walking = h != 0f || v != 0f;
-
-        // Tell the animator whether or not the player is walking.
-        anim.SetBool ("IsWalking", walking);
-    }
 }
